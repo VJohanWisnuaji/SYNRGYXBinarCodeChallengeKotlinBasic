783b3fc8fa5d605447f0ed57d9d7eea82bd08408
VJohanWisnuaji <wisnuaji.johan@gmail.com>
2020-07-10 11:12:55 +0700

    Hampir selesai bag 2

--- src/PembayaranAkhir.kt
+++ src/PembayaranAkhir.kt
@@ -2,11 +2,13 @@ import kotlin.math.roundToInt
 
 class PembayaranAkhir(var nama: String = "") {
 
+
     fun cekDiskon() {
         if (Barang.totalHarga < 50000) {
             pembayaranAkhir()
-        } else {
-            var sisa = Barang.totalHarga - (Barang.totalHarga * 0.095)
+        } else if (nama != "Non Member"){
+
+            val sisa = Barang.totalHarga - (Barang.totalHarga * 0.095)
             Barang.totalHarga = sisa.roundToInt()
             println("-----------------------------")
             println(
@@ -17,24 +19,44 @@ class PembayaranAkhir(var nama: String = "") {
             println("$nama cukup bayar Rp. $sisa")
             pembayaranAkhir()
         }
+        else {
+
+            val sisa = Barang.totalHarga - (Barang.totalHarga * 0.041)
+            Barang.totalHarga = sisa.roundToInt()
+            println("-----------------------------")
+            println(
+                """Selamat! Karena total belanjaan 
+            |lebih besar dari Rp. 50000 maka mendapatkan  
+            |potongan sebesar 4.1%""".trimMargin()
+            )
+            println("$nama cukup bayar Rp. $sisa")
+            pembayaranAkhir()
+        }
     }
 
     fun pembayaranAkhir() {
-        println("-----------------------------")
-        println("Checkout Pelanggan Member ${nama}")
-        println("-----------------------------")
-        println("Total Belanja : Rp. ${Barang.totalHarga}")
-        print("Input uang cumtomer: Rp. ")
-        var uangCostumer = readLine()!!.toInt()
-        println("-----------------------------")
-        var kembalian = uangCostumer % Barang.totalHarga
-        if (kembalian == 0) {
-            println("-TERIMA KASIH-")
-        } else {
+        try {
             println("-----------------------------")
-            println("Kembalian: Rp. $kembalian")
+            println("Checkout Pelanggan Member ${nama}")
             println("-----------------------------")
-            println("-TERIMA KASIH-")
+            println("Total Belanja : Rp. ${Barang.totalHarga}")
+            print("Input uang cumtomer: Rp. ")
+            val uangCostumer = readLine()!!.toInt()
+            println("-----------------------------")
+            val kembalian = uangCostumer % Barang.totalHarga
+            if (kembalian == 0) {
+                println("-TERIMA KASIH-")
+            } else {
+                println("-----------------------------")
+                println("Kembalian: Rp. $kembalian")
+                println("-----------------------------")
+                println("-TERIMA KASIH-")
+            }
+        } catch (e: NumberFormatException) {
+            println("Inputan jumlah salah ulangi kembali")
+            println("")
+            println("")
+            pembayaranAkhir()
         }
     }
 }
--- src/TransaksiNonMember.kt
+++ src/TransaksiNonMember.kt
@@ -1,41 +1,55 @@
+import java.lang.NumberFormatException
+
 class TransaksiNonMember {
 
     fun transaksi() {
-        println("-----------------------------")
-        println("Checkout Pelanggan Non Member")
-        println("-----------------------------")
-        print("Input Nama Barang: ")
-        val inputBarang = readLine()!!
-        println("Input Harga Satuan Barang: Rp. ${Barang.listBarang.get(inputBarang)}")
-        print("Input Jumlah Barang: ")
-        val inputJumlah = readLine()!!.toInt()
-        println("-----------------------------")
-        Barang.jumlahPerItem.put(inputBarang, inputJumlah)
-        var totalItem: Int = Barang.listBarang.get(inputBarang)!! * inputJumlah
-        Barang.listTotalItem.put(inputBarang, totalItem)
-        Barang.banyakItem.add(inputBarang)
+        try {
+            println("-----------------------------")
+            println("Checkout Pelanggan Non Member")
+            println("-----------------------------")
+            print("Input Nama Barang: ")
+            val inputBarang = readLine()!!
+            println("Input Harga Satuan Barang: Rp. ${Barang.listBarang.get(inputBarang)}")
+            print("Input Jumlah Barang: ")
+            val inputJumlah = readLine()!!.toInt()
+            println("-----------------------------")
+            Barang.jumlahPerItem.put(inputBarang, inputJumlah)
+            val totalItem: Int = Barang.listBarang.get(inputBarang)!! * inputJumlah
+            Barang.listTotalItem.put(inputBarang, totalItem)
+            Barang.banyakItem.add(inputBarang)
 
-        val ya = 'Y'
-        val tidak = 'T'
+            val ya = 'Y'
+            val tidak = 'T'
 
-        fun end() {
-            println("Input Barang Lagi? $ya/$tidak :")
-            val inputPilihan = readLine()!!
-            when (inputPilihan) {
-                "Y" -> {
-                    transaksi()
-                }
-                "T" -> {
-                    checkout()
-                }
+            fun end() {
+                println("Input Barang Lagi? $ya/$tidak :")
+                val inputPilihan = readLine()!!
+                when (inputPilihan) {
+                    "Y" -> {
+                        transaksi()
+                    }
+                    "T" -> {
+                        checkout()
+                    }
 
-                else -> {
-                    println("Pilihan tidak ada silahkan pilih lagi")
-                    end()
+                    else -> {
+                        println("Pilihan tidak ada silahkan pilih lagi")
+                        end()
+                    }
                 }
             }
+            end()
+        } catch (e: KotlinNullPointerException) {
+            println("Barang tidak terdaftar dalam katalog silahkan masukkan kembali")
+            println("")
+            println("")
+            transaksi()
+        } catch (e: NumberFormatException) {
+            println("Inputan jumlah salah ulangi kembali")
+            println("")
+            println("")
+            transaksi()
         }
-        end()
     }
 
     fun checkout() {
